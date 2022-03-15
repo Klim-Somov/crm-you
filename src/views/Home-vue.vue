fe6f6cf8f74ad8c9f8e595221890950aa916fd01
Klim <klim_somov@mail.ru>
2022-03-15 10:13:48 +0300

    resolve problems with errors in console

--- src/views/Home-vue.vue
+++ src/views/Home-vue.vue
@@ -1,64 +1,45 @@
 <template>
-    <div class="app-page ">
+  <div class="app-page">
+    <div>
+      <div class="page-title">
+        <h3>Счет</h3>
 
-          <div>
-            <div class="page-title">
-              <h3>Счет</h3>
+        <button class="btn waves-effect waves-light btn-small">
+          <i class="material-icons">refresh</i>
+        </button>
+      </div>
+      <loader v-if="loading" />
+      <div v-else class="row">
+        <Home-bill :rates="currency.rates" />
 
-              <button class="btn waves-effect waves-light btn-small">
-                <i class="material-icons">refresh</i>
-              </button>
-            </div>
-          <loader v-if="loading" />
-            <div class="row">
-          <Home-bill 
-           :rates="currency.rates"  
-          />    
-             
-          <Home-currensy 
-          :rates="currency.rates"
-          :date="date"
-          />
-           
-            </div>
-          </div>
-
-        </div>
+        <Home-currensy :rates="currency.rates" :date="date" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
-import HomeCurrensy from '@/components/Home-currensy'
-import HomeBill from '@/components/Home-bill.vue'
-import Loader from '@/components/Loader-vue.vue'
+import HomeCurrensy from "@/components/Home-currensy";
+import HomeBill from "@/components/Home-bill.vue";
+import Loader from "@/components/Loader-vue.vue";
 
 export default {
- name: 'home-vue', 
- data() {
-   return {
-loading: true,
-currensy: null,
-date: new Date()
-   }
- },
- async mounted() {
-  this.currency = await this.$store.dispatch('fetchCurrency');
-  
-  this.loading = false
-  await this.$store.dispatch('getUserid')
- 
- },
-
-
-components: {HomeBill, HomeCurrensy, Loader }, 
-
-
-
-}
-
-
+  name: "home-vue",
+  data() {
+    return {
+      loading: true,
+      currency: null,
+      date: new Date(),
+    };
+  },
+  async mounted() {
+    this.currency = await this.$store.dispatch("fetchCurrency");
+    this.loading = false;
+    await this.$store.dispatch("getUserid");
+  },
+  components: { HomeBill, HomeCurrensy, Loader },
+};
 </script>
 
 <style>
-
 </style>
