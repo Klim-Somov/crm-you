e55885aca4f5e036e5295033aadf9e68022fdb25
Klim <klim_somov@mail.ru>
2022-03-21 19:53:14 +0300

    complete validation for categorie edit

--- src/components/CategoryEdit.vue
+++ src/components/CategoryEdit.vue
@@ -17,18 +17,37 @@
           <label>Выберите категорию</label>
         </div>
 
-        <div class="input-field">
-          <input type="text" id="name" />
+         <div class="input-field">
+          <input
+            v-model="title"
+            id="name"
+            type="text"
+            :class="{ invalid: $v.title.$dirty && !$v.title.required }"
+          />
+
           <label for="name">Название</label>
-          <span class="helper-text invalid">TITLE</span>
+          <span
+            v-if="$v.title.$dirty && !$v.title.required"
+            class="helper-text invalid"
+            >Введите название категории</span
+          >
         </div>
 
-        <div class="input-field">
-          <input id="limit" type="number" />
+       <div class="input-field">
+          <input
+            id="limit"
+            type="number"
+            v-model.number="limit"
+            :class="{ invalid: $v.limit.$dirty && !$v.limit.minValue }"
+          />
           <label for="limit">Лимит</label>
-          <span class="helper-text invalid">LIMIT</span>
+          <span
+            v-if="$v.limit.$dirty && !$v.limit.minValue"
+            class="helper-text invalid"
+          >
+            Минимальное заначение{{ $v.limit.$params.minValue.min }}
+          </span>
         </div>
-
         <button class="btn waves-effect waves-light" type="submit">
           Обновить
           <i class="material-icons right">send</i>
@@ -40,7 +59,7 @@
 
 <script>
 import M from "materialize-css/dist/js/materialize.min";
-
+import { required, minValue } from "vuelidate/lib/validators";
 export default {
   props: {
     categories: {
@@ -50,11 +69,18 @@ export default {
   },
   data() {
     return {
+       title: "",
+      limit: 100,
       select: null
     };
   },
+    validations: {
+    title: { required },
+    limit: { minValue: minValue(100) },
+  },
   mounted() {
     this.select = M.FormSelect.init(this.$refs.select);
+      M.updateTextFields();
   },
   destroyed() {
     if (this.select && this.select.destroy ) {
